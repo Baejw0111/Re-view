cf816615dc8e6a5917cff436d2b859bd3f13efa6
Baejw0111 <bjw0111@naver.com>
2024-06-06 23:56:06 +0900

    feat(BE): 이미지 파일 처리 기능 추가 완료
    - 각 API에 파일 업로드, 삭제 기능 추가
    - 이미지 파일 저장용 폴더인 public 추가

--- back/server.js
+++ back/server.js
@@ -2,7 +2,10 @@
 import express from "express"; // 서버 구축용 프레임워크
 import cors from "cors"; // cors 관리
 import morgan from "morgan"; // 로그 출력용
+import multer from "multer"; // 파일 업로드 처리용
 import mongoose from "mongoose"; // MongoDB와 연결
+import path from "path"; // 파일 경로 처리용
+import fs from "fs"; // 파일 삭제용
 import "dotenv/config"; // .env 파일에서 바로 환경 변수 로드
 
 const app = express(); // express 인스턴스 생성
@@ -12,7 +15,24 @@ const { PORT, MONGO_URI } = process.env; // 로드된 환경변수는 process.en
 app.use(express.json()); //json 데이터 파싱
 app.use(express.urlencoded({ extended: true })); // form 데이터(x-www-form-urlencoded) 파싱(extended 옵션 정의 안해주면 에러 터짐)
 app.use(cors()); // cors 에러 방지
-app.use(morgan("dev"));
+app.use(morgan("dev")); // 로그 출력
+app.use("/public", express.static("public")); // url을 통해 public 폴더의 파일들에 접근 가능하게 함
+
+// 파일 업로드 처리
+const upload = multer({
+  storage: multer.diskStorage({
+    // 파일 저장 위치 설정
+    destination: function (req, file, cb) {
+      cb(null, path.join("public"));
+    },
+    // 파일 이름 설정
+    filename: function (req, file, cb) {
+      const ext = path.extname(file.originalname); // 파일 확장자 추출
+      const originalName = path.basename(file.originalname, ext); // 파일 이름 추출
+      cb(null, path.join(originalName + Date.now() + ext)); // 파일 이름 설정
+    },
+  }),
+});
 
 mongoose
   .connect(MONGO_URI)
@@ -60,6 +80,7 @@ const ReviewModel = db.model(
     author: { type: String, default: "작성자" },
     uploadTime: { type: Date, default: Date.now },
     title: { type: String, default: "" },
+    image: { type: String, default: "" },
     comments: { type: String, default: "" },
     ratings: { type: [Number], default: [0] },
     tags: { type: [String], default: [] },
@@ -102,57 +123,107 @@ app.get("/review/:id", async (req, res) => {
 
 // 리뷰 등록 API
 app.post("/review", async (req, res) => {
-  const { author, uploadTime, title, comments, ratings, tags } = req.body;
+  // 리뷰 등록에 문제가 있을 시 이미지가 서버에 저장되는 것을 막기 위해 multer 미들웨어를 수동으로 처리
+  const uploader = upload.single("image");
 
-  if (!author || !uploadTime || !title || !comments || !ratings || !tags) {
-    return res.status(400).json({ message: "모든 필드를 채워주세요." });
-  }
+  uploader(req, res, async (err) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ message: "파일 업로드 중 에러 발생", error: err });
+    }
 
-  try {
-    const reviewData = new ReviewModel({
-      author,
-      uploadTime,
-      title,
-      comments,
-      ratings,
-      tags,
-    });
-    await reviewData.save(); // 데이터 저장
-    res.status(201).json({ message: "리뷰가 성공적으로 등록되었습니다." });
-  } catch (error) {
-    console.error("리뷰 등록 중 에러 발생:", error);
-    res.status(500).json({ message: "서버 에러가 발생했습니다." });
-  }
+    // 필드 검증
+    const { author, uploadTime, title, comments, ratings, tags } = req.body;
+    if (
+      !author ||
+      !uploadTime ||
+      !title ||
+      !comments ||
+      !ratings ||
+      !tags ||
+      !req.file
+    ) {
+      // 이미지 파일이 이미 업로드되었다면 삭제
+      if (req.file) {
+        fs.unlinkSync(req.file.path);
+      }
+      return res
+        .status(400)
+        .json({ message: "모든 필드를 채워주세요.", req: req.body });
+    }
+
+    try {
+      const reviewData = new ReviewModel({
+        author,
+        image: req.file.path,
+        uploadTime,
+        title,
+        comments,
+        ratings,
+        tags,
+      });
+      await reviewData.save();
+      res.status(201).json({ message: "리뷰가 성공적으로 등록되었습니다." });
+    } catch (error) {
+      console.error("리뷰 등록 중 에러 발생:", error);
+      res.status(500).json({ message: "서버 에러가 발생했습니다." });
+    }
+  });
 });
 
 // 특정 리뷰 수정 API
 app.patch("/review/:id", async (req, res) => {
   const { id } = req.params;
   const updateData = req.body; // 클라이언트로부터 받은 업데이트할 데이터
+  const uploader = upload.single("image"); // 이미지 파일을 처리
 
-  try {
-    const reviewData = await ReviewModel.findById(id);
-    if (!reviewData) {
-      return res.status(404).json({ message: "리뷰가 존재하지 않습니다." });
+  uploader(req, res, async (err) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ message: "이미지 업로드 중 에러 발생", error: err });
     }
 
-    // 받은 필드만 업데이트
-    for (let key in updateData) {
-      reviewData[key] = updateData[key];
-    }
+    try {
+      const reviewData = await ReviewModel.findById(id);
+      if (!reviewData) {
+        return res.status(404).json({ message: "리뷰가 존재하지 않습니다." });
+      }
 
-    await reviewData.save(); // 수정된 데이터 저장
-    res.status(200).json({ message: "리뷰가 성공적으로 수정되었습니다." });
-  } catch (error) {
-    console.error("리뷰 수정 중 에러 발생:", error);
-    res.status(500).json({ message: "서버 에러가 발생했습니다." });
-  }
+      // 받은 필드만 업데이트
+      for (let key in updateData) {
+        reviewData[key] = updateData[key];
+      }
+
+      // 이미지 파일이 있을 경우 기존 이미지 파일 삭제 후 경로 업데이트
+      if (req.file) {
+        if (reviewData.image) {
+          fs.unlinkSync(reviewData.image); // 기존 이미지 파일 삭제
+        }
+        reviewData.image = req.file.path; // 새 이미지 파일 경로로 업데이트
+      }
+
+      await reviewData.save(); // 수정된 데이터 저장
+      res.status(200).json({ message: "리뷰가 성공적으로 수정되었습니다." });
+    } catch (error) {
+      console.error("리뷰 수정 중 에러 발생:", error);
+      res.status(500).json({ message: "서버 에러가 발생했습니다." });
+    }
+  });
 });
 
 // 특정 리뷰 삭제 API
 app.delete("/review/:id", async (req, res) => {
   const { id } = req.params;
   try {
+    const review = await ReviewModel.findById(id);
+    if (!review) {
+      return res.status(404).json({ message: "리뷰가 존재하지 않습니다." });
+    }
+    if (review.image) {
+      fs.unlinkSync(review.image); // 이미지 파일 삭제
+    }
     await ReviewModel.findByIdAndDelete(id);
     res.status(200).json({ message: "리뷰가 성공적으로 삭제되었습니다." });
   } catch (error) {
