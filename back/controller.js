cc13cde25fbb64be6ed908c7b8fa717b7276a701
Baejw0111 <bjw0111@naver.com>
2024-06-10 21:28:58 +0900

    refactor(BE): server.js 리팩토링
    - 기존의 server.js에서 코드들을 분리해 controller.js와 model.js 생성
    - controller.js: 비즈니스 로직 코드
    - model.js: MongoDB 연결 설정 및 스키마 정의

---
+++ back/controller.js
@@ -0,0 +1,186 @@
+// 비즈니스 로직 코드
+
+import multer from "multer"; // 파일 업로드용
+import path from "path"; // 파일 경로 설정용
+import fs from "fs"; // 파일 삭제용
+import { ReviewModel } from "./model.js";
+
+// 파일 업로드 처리
+const upload = multer({
+  storage: multer.diskStorage({
+    // 파일 저장 위치 설정
+    destination: function (req, file, cb) {
+      cb(null, path.join("public"));
+    },
+    // 파일 이름 설정
+    filename: function (req, file, cb) {
+      const ext = path.extname(file.originalname); // 파일 확장자 추출
+      const originalName = path.basename(file.originalname, ext); // 파일 이름 추출
+      cb(null, path.join(originalName + Date.now() + ext)); // 파일 이름 설정
+    },
+  }),
+});
+
+/**
+ * 리뷰 전체 조회
+ * @param {*} req 요청
+ * @param {*} res 응답
+ * @returns 리뷰 전체 조회 결과
+ */
+export const getReviews = async (req, res) => {
+  try {
+    const reviews = await ReviewModel.find();
+    res.json(reviews);
+  } catch (error) {
+    console.error("리뷰 조회 중 에러 발생:", error);
+    res.status(500).json({ message: "서버 에러가 발생했습니다." });
+  }
+};
+
+// 특정 리뷰 조회
+
+/**
+ * 특정 리뷰 조회
+ * @param {*} req 요청
+ * @param {*} res 응답
+ * @returns 특정 리뷰 조회 결과
+ */
+export const getReviewsById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const reviewData = await ReviewModel.findById(id);
+    if (!reviewData) {
+      return res.status(404).json({ message: "리뷰가 존재하지 않습니다." });
+    }
+    res.json(reviewData);
+  } catch (error) {
+    console.error("리뷰 조회 중 에러 발생:", error);
+    res.status(500).json({ message: "서버 에러가 발생했습니다." });
+  }
+};
+
+/**
+ * 리뷰 등록
+ * @param {*} req 요청
+ * @param {*} res 응답
+ * @returns 리뷰 등록 결과
+ */
+export const createReview = async (req, res) => {
+  // 리뷰 등록에 문제가 있을 시 이미지가 서버에 저장되는 것을 막기 위해 multer 미들웨어를 수동으로 처리
+  const uploader = upload.single("image");
+
+  uploader(req, res, async (err) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ message: "파일 업로드 중 에러 발생", error: err });
+    }
+
+    // 필드 검증
+    const { author, uploadTime, title, comments, ratings, tags } = req.body;
+    if (
+      !author ||
+      !uploadTime ||
+      !title ||
+      !comments ||
+      !ratings ||
+      !tags ||
+      !req.file
+    ) {
+      // 이미지 파일이 이미 업로드되었다면 삭제
+      if (req.file) {
+        fs.unlinkSync(req.file.path);
+      }
+      return res
+        .status(400)
+        .json({ message: "모든 필드를 채워주세요.", req: req.body });
+    }
+
+    try {
+      const reviewData = new ReviewModel({
+        author,
+        image: req.file.path,
+        uploadTime,
+        title,
+        comments,
+        ratings,
+        tags,
+      });
+      await reviewData.save();
+      res.status(201).json({ message: "리뷰가 성공적으로 등록되었습니다." });
+    } catch (error) {
+      console.error("리뷰 등록 중 에러 발생:", error);
+      res.status(500).json({ message: "서버 에러가 발생했습니다." });
+    }
+  });
+};
+
+/**
+ * 리뷰 수정
+ * @param {*} req 요청
+ * @param {*} res 응답
+ * @returns 리뷰 수정 결과
+ */
+export const updateReview = async (req, res) => {
+  const { id } = req.params;
+  const uploader = upload.single("image"); // 이미지 파일을 처리
+
+  uploader(req, res, async (err) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ message: "이미지 업로드 중 에러 발생", error: err });
+    }
+
+    try {
+      const reviewData = await ReviewModel.findById(id);
+      if (!reviewData) {
+        return res.status(404).json({ message: "리뷰가 존재하지 않습니다." });
+      }
+
+      // 받은 필드만 업데이트
+      const updateData = req.body; // 클라이언트로부터 받은 업데이트할 데이터
+      for (let key in updateData) {
+        reviewData[key] = updateData[key];
+      }
+
+      // 이미지 파일이 있을 경우 기존 이미지 파일 삭제 후 경로 업데이트
+      if (req.file) {
+        if (reviewData.image) {
+          fs.unlinkSync(reviewData.image); // 기존 이미지 파일 삭제
+        }
+        reviewData.image = req.file.path; // 새 이미지 파일 경로로 업데이트
+      }
+
+      await reviewData.save(); // 수정된 데이터 저장
+      res.status(200).json({ message: "리뷰가 성공적으로 수정되었습니다." });
+    } catch (error) {
+      console.error("리뷰 수정 중 에러 발생:", error);
+      res.status(500).json({ message: "서버 에러가 발생했습니다." });
+    }
+  });
+};
+
+/**
+ * 리뷰 삭제
+ * @param {*} req 요청
+ * @param {*} res 응답
+ * @returns 리뷰 삭제 결과
+ */
+export const deleteReview = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const review = await ReviewModel.findById(id);
+    if (!review) {
+      return res.status(404).json({ message: "리뷰가 존재하지 않습니다." });
+    }
+    if (review.image) {
+      fs.unlinkSync(review.image); // 이미지 파일 삭제
+    }
+    await ReviewModel.findByIdAndDelete(id);
+    res.status(200).json({ message: "리뷰가 성공적으로 삭제되었습니다." });
+  } catch (error) {
+    console.error("리뷰 삭제 중 에러 발생:", error);
+    res.status(500).json({ message: "서버 에러가 발생했습니다." });
+  }
+};
---
+++ back/model.js
@@ -0,0 +1,71 @@
+// MongoDB 연결 설정 및 스키마 정의
+
+import mongoose from "mongoose"; // MongoDB와 연결
+
+const { MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB에 연결되었습니다."))
+  .catch((err) => console.error("MongoDB 연결 에러:", err));
+/*
+참고로 이전에는 connect 함수 안에 여러 옵션들을 덕지덕지 달아놔야 했었지만
+mongoose 6.0 버전 이상부터는 해당 옵션들이 기본값이 되어 따로 명시하지 않아도 된다.
+*/
+
+const db = mongoose.connection.useDb("mainDB");
+
+// 변경 시 FE의 interface.ts도 변경할 것
+/**
+ * 유저 모델
+ * @type {mongoose.Model}
+ * @property {string} name - 사용자 이름
+ * @property {string} email - 사용자 이메일
+ * @property {string} password - 사용자 비밀번호
+ */
+export const UserModel = db.model(
+  "User",
+  new mongoose.Schema({
+    name: { type: String, default: "사용자" },
+    email: { type: String, default: "" },
+    password: { type: String, default: "" },
+  })
+);
+
+/**
+ * 리뷰 모델
+ * @type {mongoose.Model}
+ * @property {string} author - 작성자
+ * @property {Date} uploadTime - 업로드 시간
+ * @property {string} title - 제목
+ * @property {string} image - 이미지 경로
+ * @property {string} comments - 댓글
+ * @property {number[]} ratings - 평점
+ * @property {string[]} tags - 태그
+ */
+export const ReviewModel = db.model(
+  "Review",
+  new mongoose.Schema({
+    author: { type: String, default: "작성자" },
+    uploadTime: { type: Date, default: Date.now },
+    title: { type: String, default: "" },
+    image: { type: String, default: "" },
+    comments: { type: String, default: "" },
+    ratings: { type: [Number], default: [0] },
+    tags: { type: [String], default: [] },
+  })
+);
+
+/**
+ * 태그 모델
+ * @type {mongoose.Model}
+ * @property {string} tagName - 태그 이름
+ * @property {number} appliedCount - 적용된 횟수
+ */
+export const TagModel = db.model(
+  "Tag",
+  new mongoose.Schema({
+    tagName: { type: String, default: "" },
+    appliedCount: { type: Number, default: 0 },
+  })
+);
