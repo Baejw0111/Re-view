fbfae19df6534e1b2225189940b31ea411ea01eb
Baejw0111 <bjw0111@naver.com>
2024-06-12 00:26:11 +0900

    feat(FE): 기본 레이아웃 설정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,28 @@
 import "./App.css";
-import Feed from "./components/Feed";
-import ReviewForm from "./components/ReviewForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ThemeProvider from "./components/theme/ThemeProvider";
 import ThemeToggle from "./components/theme/ThemeToggle";
+import Home from "./pages/Home";
+import Test from "./pages/Test";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <ThemeToggle />
-      <Feed />
-      <ReviewForm />
+      <Router>
+        <div className="flex">
+          <div className="flex-none w-48">
+            <Menu />
+          </div>
+          <div className="flex-1 flex justify-center">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/test" element={<Test />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
     </ThemeProvider>
   );
 }
