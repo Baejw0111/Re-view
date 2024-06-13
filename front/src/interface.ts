b7f013a64f9e2a024f0cbe644827d44ff9861146
Baejw0111 <bjw0111@naver.com>
2024-06-13 21:41:50 +0900

    feat(FE): interface.ts 수정
    - 리뷰 정보에 image 속성 추가
    - JSDoc 적용

--- front/src/interface.ts
+++ front/src/interface.ts
@@ -1,21 +1,39 @@
-// 유저 정보
+/**
+ * @description 유저 정보
+ * @param name 유저 이름
+ * @param email 유저 이메일
+ * @param password 유저 비밀번호
+ */
 export interface User {
   name: string;
   email: string;
   password: string;
 }
 
-// 리뷰 정보
+/**
+ * @description 리뷰 정보
+ * @param author 리뷰 작성자
+ * @param uploadTime 리뷰 업로드 시간
+ * @param title 리뷰 제목
+ * @param image 리뷰 이미지
+ * @param comments 리뷰 내용
+ * @param ratings 리뷰 평점
+ */
 export interface Review {
   author: string;
   uploadTime: Date;
   title: string;
+  image: string;
   comments: string;
   ratings: number[];
   tags: string[];
 }
 
-// 태그 정보
+/**
+ * @description 태그 정보
+ * @param tagName 태그 이름
+ * @param appliedCount 태그 적용 횟수
+ */
 export interface Tag {
   tagName: string;
   appliedCount: number;
