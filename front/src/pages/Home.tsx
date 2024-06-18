e8387561cf837189ce41eb65baf0f36a631c3f77
Baejw0111 <bjw0111@naver.com>
2024-06-18 16:59:30 +0900

    feat(FE): Home, Test 페이지 생성

---
+++ front/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Feed from "../components/Feed";
+import ReviewModalButton from "../components/ReviewModalButton";
+
+export default function Home() {
+  return (
+    <>
+      <ReviewModalButton />
+      <Feed />
+    </>
+  );
+}
---
+++ front/src/pages/Test.tsx
@@ -0,0 +1,10 @@
+import ReviewModalButton from "../components/ReviewModalButton";
+
+export default function Test() {
+  return (
+    <>
+      <div className="text-2xl md:text-3xl font-bold m-4">Test Page</div>
+      <ReviewModalButton />
+    </>
+  );
+}
