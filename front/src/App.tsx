baf517a1906166390035458bd0d10020547a462f
Baejw0111 <bjw0111@naver.com>
2024-06-18 20:22:06 +0900

    feat(FE): react-query 적용 및 코드 리팩토링, 리뷰 작성 버튼 추가
    - Home 컴포넌트를 Feed 컴포넌트로 교체
    - Feed 컴포넌트에서 리뷰 표시해주는 컴포넌트인 Review 분리

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,29 +1,35 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ThemeProvider from "./components/theme/ThemeProvider";
 import ThemeToggle from "./components/theme/ThemeToggle";
-import Home from "./pages/Home";
+import Feed from "./pages/Feed";
 import Test from "./pages/Test";
 import Menu from "./components/Menu";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <ThemeToggle />
-      <Router>
-        <div className="flex">
-          <div className="flex-none w-48">
-            <Menu />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <ThemeToggle />
+        <Router>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <div className="flex">
+            <div className="flex-none w-48">
+              <Menu />
+            </div>
+            <div className="flex-1 flex justify-center">
+              <Routes>
+                <Route path="/" element={<Feed />} />
+                <Route path="/test" element={<Test />} />
+              </Routes>
+            </div>
           </div>
-          <div className="flex-1 flex justify-center">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/test" element={<Test />} />
-            </Routes>
-          </div>
-        </div>
-      </Router>
-    </ThemeProvider>
+        </Router>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
---
+++ front/src/pages/Feed.tsx
@@ -0,0 +1,16 @@
+import Reviews from "../components/Reviews";
+import ReviewModalButton from "../components/ReviewModalButton";
+
+export default function Feed() {
+  return (
+    <>
+      <ReviewModalButton />
+      <div className="w-full max-w-6xl mx-auto px-4 md:px-6 py-12 md:py-16">
+        <div className="flex items-center justify-between mb-8">
+          <h1 className="text-2xl md:text-3xl font-bold">Feed</h1>
+        </div>
+        <Reviews />
+      </div>
+    </>
+  );
+}
