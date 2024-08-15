3755e737400a4ae127b83fb9f3330f62a409ff81
Baejw0111 <bjw0111@naver.com>
2024-08-15 17:38:40 +0900

    feat(FE): Menu 컴포넌트 수정
    - nav 태그로 감싸고, css 수정

--- front/src/components/Menu.tsx
+++ front/src/components/Menu.tsx
@@ -2,18 +2,21 @@ import { Link } from "react-router-dom";
 
 export default function Menu() {
   return (
-    <div className="fixed top-0 left-0 w-48 h-full bg-gray-800 text-white p-4">
-      <ul>
-        <li className="my-4 hover:text-blue-500 hover:underline font-bold">
+    <nav className="absolute left-28 w-64">
+      <ul className="flex justify-between">
+        <li className="hover:text-blue-500 hover:underline font-bold">
           <Link to="/test">Test</Link>
         </li>
-        <li className="my-4 hover:text-blue-500 hover:underline font-bold">
+        <li className="hover:text-blue-500 hover:underline font-bold">
           <Link to="/">Home</Link>
         </li>
-        <li className="my-4 hover:text-blue-500 hover:underline font-bold">
+        <li className="hover:text-blue-500 hover:underline font-bold">
           <Link to="/review">Review</Link>
         </li>
+        <li className="hover:text-blue-500 hover:underline font-bold">
+          <Link to="/oauth/kakao">Log In</Link>
+        </li>
       </ul>
-    </div>
+    </nav>
   );
 }
