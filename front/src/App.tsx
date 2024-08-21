61dcb9f564206a5240e8b8fbf15f521f97fc8956
Baejw0111 <bjw0111@naver.com>
2024-08-21 14:55:38 +0900

    feat(FE): Redux 스토어 설정 및 App 컴포넌트에 Provider 추가

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,28 +2,32 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Provider } from "react-redux";
 import ThemeProvider from "./components/theme/ThemeProvider";
 import Feed from "./pages/Feed";
 import Test from "./pages/Test";
 import Header from "./components/Header";
 import Review from "./pages/Review";
 import Authorization from "./pages/Authorization";
+import store from "./store/store";
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <Router>
-          <ReactQueryDevtools initialIsOpen={false} />
-          <Header />
-          <Routes>
-            <Route path="/test" element={<Test />} />
-            <Route path="/" element={<Feed />} />
-            <Route path="/review" element={<Review />} />
-            <Route path="/oauth/kakao" element={<Authorization />} />
-          </Routes>
-        </Router>
+        <Provider store={store}>
+          <Router>
+            <ReactQueryDevtools initialIsOpen={false} />
+            <Header />
+            <Routes>
+              <Route path="/test" element={<Test />} />
+              <Route path="/" element={<Feed />} />
+              <Route path="/review" element={<Review />} />
+              <Route path="/oauth/kakao" element={<Authorization />} />
+            </Routes>
+          </Router>
+        </Provider>
       </ThemeProvider>
     </QueryClientProvider>
   );
