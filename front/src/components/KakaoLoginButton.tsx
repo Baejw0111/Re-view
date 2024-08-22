270ab3fb7c23ad8efdde357a0ed1a471e52372de
Baejw0111 <bjw0111@naver.com>
2024-08-22 18:12:01 +0900

    feat(FE): 카카오 로그인 버튼 추가
    - 카카오 로그인 인가 코드 발급 요청 프로세스를 버튼으로 분리
    - 버튼 클릭 시 카카오 인증 페이지로 리다이렉트

---
+++ front/src/components/KakaoLoginButton.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+
+export default function KakaoLoginButton() {
+  const KAKAO_OAUTH_URL =
+    "https://kauth.kakao.com/oauth/authorize?response_type=code";
+  const KAKAO_REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
+  const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+
+  return (
+    <Button
+      className="h-9 w-[140px] font-semibold bg-yellow-300 text-black hover:bg-yellow-400"
+      onClick={() =>
+        (window.location.href = `${KAKAO_OAUTH_URL}&client_id=${KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}`)
+      }
+    >
+      카카오 로그인
+    </Button>
+  );
+}
