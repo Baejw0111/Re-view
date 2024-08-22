34ad3b80e52911993233056ea69809a36cdfe4e6
Baejw0111 <bjw0111@naver.com>
2024-08-23 00:58:11 +0900

    feat(FE): 헤더 컴포넌트에 사용자 정보에 따라 표시되는 버튼 교체
    - 사용자 닉네임이 있는 경우 리뷰 작성 버튼, 알림 버튼, 로그아웃 버튼을 표시
    - 사용자 닉네임이 없는 경우 로그인 버튼을 표시

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,16 +1,35 @@
 import Logo from "@/components/Logo";
 import Menu from "@/components/Menu";
 import ThemeToggle from "@/components/theme/ThemeToggle";
-import ReviewModalButton from "./CreateReviewButton";
+import CreateReviewButton from "@/components/CreateReviewButton";
+import UserProfile from "@/components/UserProfile";
+import NotificationButton from "@/components/NotificationButton";
+import KakaoLoginButton from "@/components/KakaoLoginButton";
+import KakaoLogoutButton from "@/components/KakaoLogoutButton";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
 
 export default function Header() {
+  const userInfo = useSelector((state: RootState) => state.userInfo);
+
   return (
-    <header className="fixed p-0 top-0 left-0 w-full h-16 bg-gray-600 text-white flex justify-around items-center">
+    <header className="fixed p-0 top-0 left-0 w-full h-16 bg-gray-600 text-white flex justify-around items-center z-50">
       <Logo />
       <Menu />
-      <div className="absolute right-4 w-20 flex justify-between">
-        <ReviewModalButton />
+      <UserProfile />
+      <div className="absolute right-4 w-48 flex justify-between">
         <ThemeToggle />
+        {userInfo.nickname ? (
+          // 닉네임이 있으면 유저가 사용 가능한 버튼 보여주기
+          <>
+            <CreateReviewButton />
+            <NotificationButton />
+            <KakaoLogoutButton />
+          </>
+        ) : (
+          // 닉네임이 없으면 로그인 버튼 보여주기
+          <KakaoLoginButton />
+        )}
       </div>
     </header>
   );
