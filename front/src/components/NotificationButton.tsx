647ae65a7f954c26ecb3a565f713acaf03e431fa
Baejw0111 <bjw0111@naver.com>
2024-08-22 18:13:32 +0900

    feat(FE): 알림 버튼 추가
    - 추후 기능 추가 예정

---
+++ front/src/components/NotificationButton.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { Bell } from "lucide-react";
+
+export default function NotificationButton() {
+  return (
+    <Button size="icon">
+      <Bell />
+    </Button>
+  );
+}
