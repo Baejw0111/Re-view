092298f4a8993b06beb93523a25d8dda8e75d22f
Baejw0111 <bjw0111@naver.com>
2024-08-29 12:47:49 +0900

    refactor(FE): 피드 페이지의 주소를 `/`에서 `/feed`로 변경

--- front/src/features/common/Menu.tsx
+++ front/src/features/common/Menu.tsx
@@ -5,7 +5,7 @@ export default function Menu() {
     <nav className="absolute left-28 w-64">
       <ul className="flex justify-between">
         <li className="hover:text-gray-500 dark:hover:text-gray-300 font-bold">
-          <Link to="/">피드</Link>
+          <Link to="/feed">피드</Link>
         </li>
         <li className="hover:text-gray-500 dark:hover:text-gray-300 font-bold">
           <Link to="/test">테스트 페이지</Link>
