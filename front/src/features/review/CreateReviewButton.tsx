b74ab986711919a8b6f911358e32653be8f8b765
Baejw0111 <bjw0111@naver.com>
2024-09-05 12:58:32 +0900

    chore(FE): Menu 컴포넌트에서 쓸모없는 부분 삭제 및 리뷰 작성 버튼에 역할 부여
    - 이제 CreateReviewButton을 누르면 리뷰 작성 페이지로 이동함

--- front/src/features/review/CreateReviewButton.tsx
+++ front/src/features/review/CreateReviewButton.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/shared/shadcn-ui/button";
 import { PencilLine } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateReviewButton() {
+  const navigate = useNavigate();
   return (
-    <Button variant="ghost" size="icon">
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => navigate("/create-review")}
+    >
       <PencilLine />
     </Button>
   );
