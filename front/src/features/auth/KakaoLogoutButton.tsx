8729ca6fc4f49069988cef4b2172cb2ed9860d4c
Baejw0111 <bjw0111@naver.com>
2024-09-09 00:13:14 +0900

    feat(FE): 카카오 로그아웃 기능 개선 및 상수 파일 추가
    - `KakaoLogoutButton` 컴포넌트에서 로그아웃 시 카카오계정과 함께 로그아웃 하는 기능 추가
    - `constants.ts` 파일에 `KAKAO_LOGOUT_REDIRECT_URI` 상수 추가

--- front/src/features/auth/KakaoLogoutButton.tsx
+++ front/src/features/auth/KakaoLogoutButton.tsx
@@ -3,12 +3,16 @@ import { logOutKakao } from "@/api/kakaoAuth";
 import { LogOutIcon } from "lucide-react";
 import { persistor } from "@/state/store/index";
 import TooltipWrapper from "@/shared/original-ui/TooltipWrapper";
+import {
+  KAKAO_REST_API_KEY,
+  KAKAO_LOGOUT_REDIRECT_URI,
+} from "@/shared/constants";
 
 export default function KakaoLogoutButton() {
   const handleLogOut = async () => {
     await logOutKakao();
     await persistor.purge(); // 로그아웃 시 redux-persist가 관리하는 모든 상태 초기화
-    window.location.href = "/";
+    window.location.href = `https://kauth.kakao.com/oauth/logout?client_id=${KAKAO_REST_API_KEY}&logout_redirect_uri=${KAKAO_LOGOUT_REDIRECT_URI}`;
   };
 
   return (
