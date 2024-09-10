e06fd1d0b2254855a3e4625478fe3bc9fb249dee
Baejw0111 <bjw0111@naver.com>
2024-09-10 14:47:17 +0900

    feat(BE): 유저 모델에 닉네임 필드 추가
    - `UserModel`에 `nickname` 필드 추가
    - 유저 모델 주석에 `nickname` 속성 설명 추가

--- back/utils/Model.js
+++ back/utils/Model.js
@@ -20,6 +20,7 @@ const db = mongoose.connection.useDb("mainDB");
  * 유저 모델
  * @type {mongoose.Model}
  * @property {string} kakaoId - 카카오 ID
+ * @property {string} nickname - 닉네임
  * @property {string[]} reviews - 작성한 리뷰 ID 모음
  * @property {string[]} likedReviews - 좋아요한 리뷰 ID 모음
  * @property {string[]} favoriteTags - 자주 찾는 태그 모음
