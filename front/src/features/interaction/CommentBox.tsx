bea62c11cba4440b548736cfddadea4b065bf1cb
Baejw0111 <bjw0111@naver.com>
2024-09-11 18:45:02 +0900

    feat(FE): CommentBox 컴포넌트에 댓글 삭제 기능 추가
    - `CommentBox` 컴포넌트에서 `deleteComment` API 호출을 통해 댓글 삭제 기능 구현
    - 댓글 삭제 시 `useMutation`과 `useQueryClient`를 사용하여 원래 캐시 무효화 처리 후 refetch
    - 댓글 작성자 정보 조회를 위해 `fetchUserInfoById` API 호출
    - 댓글 정보 prop 이름을 `comment`에서 `commentInfo`로 변경
    - 컴포넌트에 커서 호버링 시 발생하는 효과 삭제
    - 댓글 삭제, 수정, 좋아요 버튼 추가

--- front/src/features/interaction/CommentBox.tsx
+++ front/src/features/interaction/CommentBox.tsx
@@ -1,16 +1,37 @@
 import { Avatar, AvatarImage, AvatarFallback } from "@/shared/shadcn-ui/avatar";
 import { CommentInfo, UserInfo } from "@/shared/types/interface";
 import { useQuery } from "@tanstack/react-query";
-import { fetchUserInfoById } from "@/api/interaction";
+import { fetchUserInfoById, deleteComment } from "@/api/interaction";
+import { Trash, Pencil, Heart } from "lucide-react";
+import { Button } from "@/shared/shadcn-ui/button";
+import { useMutation } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
 
-export default function CommentBox({ comment }: { comment: CommentInfo }) {
+export default function CommentBox({
+  commentInfo,
+}: {
+  commentInfo: CommentInfo;
+}) {
+  const { id: reviewId } = useParams();
+  const queryClient = useQueryClient();
+
+  // 댓글 작성자 정보 가져오기
   const { data } = useQuery<UserInfo>({
-    queryKey: ["user", comment.authorId],
-    queryFn: () => fetchUserInfoById(comment.authorId),
+    queryKey: ["user", commentInfo.authorId],
+    queryFn: () => fetchUserInfoById(commentInfo.authorId),
+  });
+
+  // 댓글 삭제
+  const { mutate: deleteCommentMutate } = useMutation({
+    mutationFn: (commentId: string) => deleteComment(commentId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["comments", reviewId] });
+    },
   });
 
   return (
-    <div className="flex items-start gap-4 rounded-md hover:bg-muted">
+    <div className="flex items-start gap-4">
       <Avatar className="h-7 w-7">
         <AvatarImage src={data?.thumbnailImage} alt={data?.nickname} />
         <AvatarFallback>{data?.nickname.slice(0, 1)}</AvatarFallback>
@@ -19,12 +40,29 @@ export default function CommentBox({ comment }: { comment: CommentInfo }) {
         <div className="flex items-center justify-between">
           <div className="font-semibold">{data?.nickname}</div>
           <div className="text-xs text-muted-foreground p-2">
-            {new Date(comment.uploadTime).toLocaleDateString()}
+            {new Date(commentInfo.uploadTime).toLocaleDateString()}
           </div>
         </div>
-        <p className="text-muted-foreground whitespace-pre-wrap break-all">
-          {comment.content}
-        </p>
+        <div className="flex items-center justify-between gap-2">
+          <p className="text-muted-foreground whitespace-pre-wrap break-all">
+            {commentInfo.content}
+          </p>
+          <div className="flex items-center gap-2">
+            <Button variant="ghost" size="icon">
+              <Heart className="w-4 h-4 text-muted-foreground" />
+            </Button>
+            <Button variant="ghost" size="icon">
+              <Pencil className="w-4 h-4 text-muted-foreground" />
+            </Button>
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={() => deleteCommentMutate(commentInfo._id)}
+            >
+              <Trash className="w-4 h-4 text-muted-foreground" />
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
