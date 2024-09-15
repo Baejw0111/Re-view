91dbb78bcf3daf91bd1a7a2f37ba1d8e1ffb57c9
Baejw0111 <bjw0111@naver.com>
2024-09-16 01:22:55 +0900

    feat(FE): ReviewRatingSign 컴포넌트 개선
    - `ReviewRatingSign` 컴포넌트에 `rating` 기본값 0 추가해 에러 방지
    - 평점에 따라 텍스트 색상이 변경되도록 로직 수정

--- front/src/features/review/ReviewRatingSign.tsx
+++ front/src/features/review/ReviewRatingSign.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/shared/lib/utils";
 
 export default function ReviewRatingSign({
   className,
-  rating,
+  rating = 0,
 }: {
   className?: string;
   rating: number;
@@ -19,27 +19,27 @@ export default function ReviewRatingSign({
   // 4.5	아쿠아마린	#7fffd4
   // 5	딥스카이블루	#00bfff
 
-  const color: { [key: number]: string } = {
-    0: "#800000",
-    0.5: "#a52a2a",
-    1: "#ff0000",
-    1.5: "#ff7f50",
-    2: "#ff8c00",
-    2.5: "#febd3d",
-    3: "#ffd700",
-    3.5: "#adff2f",
-    4: "#04cc7b",
-    4.5: "#7fffd4",
-    5: "#00bfff",
+  const color: { [key: number]: [string, string] } = {
+    0: ["#800000", "text-white"],
+    0.5: ["#a52a2a", "text-white"],
+    1: ["#ff0000", "text-white"],
+    1.5: ["#ff7f50", "text-white"],
+    2: ["#ff8c00", "text-white"],
+    2.5: ["#febd3d", "text-white"],
+    3: ["#ffd700", "text-black"],
+    3.5: ["#adff2f", "text-black"],
+    4: ["#04cc7b", "text-black"],
+    4.5: ["#7fffd4", "text-black"],
+    5: ["#00bfff", "text-black"],
   };
 
   return (
     <div
       className={cn(
-        "flex items-center justify-center h-6 w-8 rounded-sm text-primary-foreground font-bold text-md",
+        `flex flex-shrink-0 items-center justify-center h-6 w-8 rounded-sm font-bold text-md ${color[rating][1]}`,
         className
       )}
-      style={{ backgroundColor: color[rating] }}
+      style={{ backgroundColor: color[rating][0] }}
     >
       {rating}
     </div>
