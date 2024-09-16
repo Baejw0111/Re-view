0982a8c9057353fb261063cd6d17a3e9b73de1cb
Baejw0111 <bjw0111@naver.com>
2024-09-16 14:31:45 +0900

    feat(FE): ReviewActionBar 컴포넌트에 수정 및 삭제 기능 추가
    - `ReviewActionBar` 컴포넌트에 `isAuthor` prop 추가
      - 작성자 여부에 따라 수정 및 삭제 버튼 표시
    - 공유 버튼 클릭 시 링크 복사 기능 추가
    - 작성자일 경우 수정 및 삭제 버튼 표시
    - 작성자가 아닐 경우 신고 버튼 표시

--- front/src/widgets/ReviewActionBar.tsx
+++ front/src/widgets/ReviewActionBar.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@/shared/shadcn-ui/button";
-import { Share2, Trash2, Flag } from "lucide-react";
+import { Share2, Trash2, Flag, FilePenLine } from "lucide-react";
 import { useMutation } from "@tanstack/react-query";
 import { deleteReview } from "@/api/review";
 import { useParams } from "react-router-dom";
 import TooltipWrapper from "@/shared/original-ui/TooltipWrapper";
 import LikeButton from "@/features/interaction/LikeButton";
 
-export default function ReviewActionBar() {
+export default function ReviewActionBar({ isAuthor }: { isAuthor: boolean }) {
   const { id: reviewId } = useParams();
   const { mutate: deleteReviewMutate } = useMutation({
     mutationFn: () => deleteReview(reviewId as string),
@@ -23,27 +23,47 @@ export default function ReviewActionBar() {
     <div className="flex items-center gap-2">
       <LikeButton reviewId={reviewId as string} />
       <TooltipWrapper tooltipText="공유">
-        <Button variant="ghost" size="icon">
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => {
+            navigator.clipboard.writeText(window.location.href);
+            alert("링크가 복사되었습니다.");
+          }}
+        >
           <Share2 className="w-6 h-6" />
           <span className="sr-only">공유</span>
         </Button>
       </TooltipWrapper>
-      <TooltipWrapper tooltipText="삭제">
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => deleteReviewMutate()}
-        >
-          <Trash2 className="w-6 h-6 hover:text-red-500" />
-          <span className="sr-only">삭제</span>
-        </Button>
-      </TooltipWrapper>
-      <TooltipWrapper tooltipText="신고">
-        <Button variant="ghost" size="icon" className="ml-auto">
-          <Flag className="w-6 h-6" />
-          <span className="sr-only">신고</span>
-        </Button>
-      </TooltipWrapper>
+      <div className="flex items-center gap-2 ml-auto">
+        {isAuthor ? (
+          <>
+            <TooltipWrapper tooltipText="작성글 수정">
+              <Button variant="ghost" size="icon">
+                <FilePenLine className="w-6 h-6" />
+                <span className="sr-only">수정</span>
+              </Button>
+            </TooltipWrapper>
+            <TooltipWrapper tooltipText="삭제">
+              <Button
+                variant="ghost"
+                size="icon"
+                onClick={() => deleteReviewMutate()}
+              >
+                <Trash2 className="w-6 h-6 hover:text-red-500" />
+                <span className="sr-only">삭제</span>
+              </Button>
+            </TooltipWrapper>
+          </>
+        ) : (
+          <TooltipWrapper tooltipText="신고">
+            <Button variant="ghost" size="icon">
+              <Flag className="w-6 h-6" />
+              <span className="sr-only">신고</span>
+            </Button>
+          </TooltipWrapper>
+        )}
+      </div>
     </div>
   );
 }
