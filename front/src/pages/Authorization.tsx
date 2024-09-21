3a223cee12a521f8cd6eea69f8bb5f1c1502f857
Baejw0111 <bjw0111@naver.com>
2024-09-21 17:11:55 +0900

    style(FE): Authorization 페이지의 레이아웃 및 패딩 수정

--- front/src/pages/Authorization.tsx
+++ front/src/pages/Authorization.tsx
@@ -46,7 +46,7 @@ export default function Authorization() {
 
   if (kakaoLoginMutation.isError) {
     return (
-      <div className="w-full max-w-6xl mx-auto px-4 md:px-6 py-12 md:py-16">
+      <div className="w-full px-4 py-6 md:py-4">
         <div className="text-2xl md:text-3xl font-bold m-8">
           로그인 처리 중 오류가 발생했습니다.
         </div>
