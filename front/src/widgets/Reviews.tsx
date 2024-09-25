a1aa9048c50b13b2da3b6a5a8a677ffcf83a712d
Baejw0111 <bjw0111@naver.com>
2024-09-25 21:54:13 +0900

    fix(FE): 피드 페이지에서 모달 창이 기본적으로 닫히도록 설정

--- front/src/widgets/Reviews.tsx
+++ front/src/widgets/Reviews.tsx
@@ -1,4 +1,8 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { useDispatch } from "react-redux";
+import { setIsOpen } from "@/state/store/reviewDetailOpenSlice";
 import { fetchReviewIdList } from "@/api/review";
 import ReviewCard from "@/widgets/ReviewCard";
 
@@ -11,6 +15,16 @@ export default function Reviews() {
     queryKey: ["reviewIdList"],
     queryFn: () => fetchReviewIdList(),
   });
+
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    if (queryParams.get("reviewId") === null) {
+      dispatch(setIsOpen(false));
+    }
+  }, [location.search, dispatch]);
 
   if (error) return <div>에러: {error.message}</div>;
   if (isLoading) return <div>로딩 중...</div>;
