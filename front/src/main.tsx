6c3a1a4b122f69ff8bce84175d61b904085938ec
Baejw0111 <bjw0111@naver.com>
2024-09-28 12:50:01 +0900

    feat(FE): `main.tsx`에 `react-query`와 `redux-toolkit`의 provider 적용

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App.tsx";
 import "@/index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import { persistor } from "@/state/store";
+import { PersistGate } from "redux-persist/integration/react";
+import store from "@/state/store";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
