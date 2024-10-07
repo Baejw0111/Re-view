3ee6d39a1493d2516e210ad1dbb9842e06048113
Baejw0111 <bjw0111@naver.com>
2024-10-07 13:56:04 +0900

    fix(UserSetting): 파일 삭제 로직 수정 및 프로필 이미지 업데이트
    - 유저 정보가 없거나 권한이 없을 경우 업로드된 파일 삭제 추가
    - `useDefaultProfile` 값이 문자열 "true"일 때 기본 프로필 이미지 사용하도록 수정
    - 프로필 이미지 업데이트 시 기존 이미지 삭제 로직 추가
    - `req.files`에서 `req.file`로 변경하여 단일 파일 처리

--- back/controllers/UserSetting.js
+++ back/controllers/UserSetting.js
@@ -11,18 +11,25 @@ export const updateUserInfo = asyncHandler(async (req, res) => {
 
   const userInfo = await UserModel.findOne({ kakaoId });
   if (!userInfo) {
+    deleteUploadedFiles(req.file.path);
     return res.status(404).json({ message: "유저를 찾을 수 없습니다." });
   } else if (userInfo.kakaoId !== kakaoId) {
+    deleteUploadedFiles(req.file.path);
     return res.status(403).json({ message: "유저 정보 수정 권한이 없습니다." });
   }
 
-  if (useDefaultProfile) {
+  if (useDefaultProfile.toLowerCase() === "true") {
     // 기본 프로필 사용 시
+    if (userInfo.profileImage !== "") {
+      deleteUploadedFiles([userInfo.profileImage]);
+    }
     userInfo.profileImage = ""; // 프로필 이미지 필드를 빈 문자열로 설정
-  } else if (req.files && req.files.length > 0) {
+  } else if (req.file) {
     // 사용자 지정 프로필 사용 시
-    deleteUploadedFiles(req.files.map((file) => file.path));
-    userInfo.profileImage = req.files[0].path; // 업로드된 파일 경로 저장
+    if (userInfo.profileImage !== "") {
+      deleteUploadedFiles([userInfo.profileImage]);
+    }
+    userInfo.profileImage = req.file.path; // 업로드된 파일 경로 저장
   }
 
   userInfo.nickname = newNickname;
