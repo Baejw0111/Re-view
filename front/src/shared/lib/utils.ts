9909da0447b57f5cf0187a0cc77a6941fbb469d5
Baejw0111 <bjw0111@naver.com>
2024-10-16 15:58:06 +0900

    fix(FE): 시간 계산 함수의 초 단위 표현 수정
    - 초 단위가 60초 미만일 때 '방금 전'으로 변경

--- front/src/shared/lib/utils.ts
+++ front/src/shared/lib/utils.ts
@@ -62,7 +62,7 @@ export const claculateTime = (time: string) => {
   const months = Math.floor(days / 30);
   const years = Math.floor(days / 365);
 
-  if (seconds < 60) return `${seconds}초 전`;
+  if (seconds < 60) return `방금 전`;
   if (minutes < 60) return `${minutes}분 전`;
   if (hours < 24) return `${hours}시간 전`;
   if (days < 30) return `${days}일 전`;
