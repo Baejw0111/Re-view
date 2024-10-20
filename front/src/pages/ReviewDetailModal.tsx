468681c4220baa80abb0faf9b33a1e3f0cf662b2
Baejw0111 <bjw0111@naver.com>
2024-10-20 12:32:30 +0900

    refactor(FE): 리뷰 상세 모달 및 알림 상태 관리 개선
    - `setIsOpen`을 `setIsReviewDetailOpen`으로 변경하여 명확한 상태 관리
    - 알림 상태 관리를 위한 `setIsNotificationOpen` 추가
    - `Dialog` 및 `Drawer` 컴포넌트의 열림 상태를 `isModalOpen`과 `isNotificationOpen`으로 제어
    - URL 쿼리 파라미터에 따라 모달 및 알림 상태를 업데이트

--- front/src/pages/ReviewDetailModal.tsx
+++ front/src/pages/ReviewDetailModal.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@/state/store";
-import { setIsOpen } from "@/state/store/reviewDetailOpenSlice";
+import { setIsReviewDetailOpen } from "@/state/store/reviewDetailOpenSlice";
+import { setIsNotificationOpen } from "@/state/store/notificationOpenSlice";
 import { useMediaQuery } from "@/shared/hooks/useMediaQuery";
 import {
   Dialog,
@@ -26,18 +27,21 @@ export default function ReviewDetailModal() {
   const dispatch = useDispatch();
   const location = useLocation();
   const isDesktop = useMediaQuery("(min-width: 768px)"); // md(768px) 아래의 너비는 모바일 환경으로 간주
-  const isOpen = useSelector(
-    (state: RootState) => state.reviewDetailOpen.isOpen
+  const isModalOpen = useSelector(
+    (state: RootState) => state.reviewDetailOpen.isReviewDetailOpen
+  );
+  const isNotificationOpen = useSelector(
+    (state: RootState) => state.notificationOpen.isNotificationOpen
   );
   const navigate = useNavigate();
 
   const handleClose = () => {
-    if (isOpen) {
+    if (isModalOpen) {
       /**
        * 아래의 코드는 유저가 직접 UI를 조작해 isOpen을 false로 만들 때 작동한다.
        * 코드에 의해 isOpen이 false가 될 경우 아래의 코드는 동작하지 않는다.
        */
-      dispatch(setIsOpen(false));
+      dispatch(setIsReviewDetailOpen(false));
       navigate(location.pathname);
     }
   };
@@ -45,15 +49,21 @@ export default function ReviewDetailModal() {
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
     if (queryParams.get("reviewId") === null) {
-      dispatch(setIsOpen(false));
+      dispatch(setIsReviewDetailOpen(false));
+    } else {
+      dispatch(setIsNotificationOpen(false));
+      dispatch(setIsReviewDetailOpen(true));
     }
-  }, [location.search, dispatch]);
+  }, [location.search]);
 
   if (isDesktop === null) return;
 
   if (isDesktop) {
     return (
-      <Dialog open={isOpen} onOpenChange={handleClose}>
+      <Dialog
+        open={isModalOpen && !isNotificationOpen}
+        onOpenChange={handleClose}
+      >
         <DialogContent
           className="block md:max-w-[760px] lg:max-w-[860px] h-[90vh] p-20 overflow-y-auto scrollbar-hide"
           onOpenAutoFocus={(e) => e.preventDefault()}
@@ -70,7 +80,7 @@ export default function ReviewDetailModal() {
   }
 
   return (
-    <Drawer open={isOpen} onClose={handleClose}>
+    <Drawer open={isModalOpen && !isNotificationOpen} onClose={handleClose}>
       <DrawerContent className="h-[90vh]">
         <DrawerTitle hidden></DrawerTitle>
         <DrawerDescription hidden></DrawerDescription>
