159b564b176d762a7b308e643281f2db6b2dcadc
Baejw0111 <bjw0111@naver.com>
2024-10-22 21:26:26 +0900

    refactor(FE): `LikeButton` 및 `Reviews` 컴포넌트 코드 개선
    - `LikeButton` 컴포넌트에서 `queryClient.invalidateQueries`의 `queryKey` 수정
      - `isLiked`의 `queryKey`를 `kakaoId`에서 `reviewId`로 변경
      - `userLikedList`의 `queryKey` 주석 해제 및 적용
    - `Reviews` 컴포넌트에서 `ReviewCard`의 `key`를 `index`에서 `reviewId`로 변경

--- front/src/features/interaction/LikeButton.tsx
+++ front/src/features/interaction/LikeButton.tsx
@@ -33,8 +33,8 @@ export default function LikeButton({
     mutationFn: () => likeReview(reviewId as string),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["reviewInfo", reviewId] });
-      queryClient.invalidateQueries({ queryKey: ["isLiked", kakaoId] });
-      // queryClient.invalidateQueries({ queryKey: ["userLikedList", kakaoId] });
+      queryClient.invalidateQueries({ queryKey: ["isLiked", reviewId] });
+      queryClient.invalidateQueries({ queryKey: ["userLikedList", kakaoId] });
     },
     onError: () => {
       alert("추천 실패");
@@ -45,8 +45,8 @@ export default function LikeButton({
     mutationFn: () => unlikeReview(reviewId as string),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["reviewInfo", reviewId] });
-      queryClient.invalidateQueries({ queryKey: ["isLiked", kakaoId] });
-      // queryClient.invalidateQueries({ queryKey: ["userLikedList", kakaoId] });
+      queryClient.invalidateQueries({ queryKey: ["isLiked", reviewId] });
+      queryClient.invalidateQueries({ queryKey: ["userLikedList", kakaoId] });
     },
     onError: () => {
       alert("추천 취소 실패");
--- front/src/widgets/Reviews.tsx
+++ front/src/widgets/Reviews.tsx
@@ -8,8 +8,8 @@ import ReviewCard from "@/widgets/ReviewCard";
 export default function Reviews({ reviewIdList }: { reviewIdList: string[] }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6">
-      {reviewIdList?.map((reviewId: string, index: number) => (
-        <ReviewCard key={index} reviewId={reviewId} />
+      {reviewIdList?.map((reviewId: string) => (
+        <ReviewCard key={reviewId} reviewId={reviewId} />
       ))}
     </div>
   );
