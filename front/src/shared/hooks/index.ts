ef9a8b518e8bc953faea7c6fdcb3ca7d05c7709f
Baejw0111 <bjw0111@naver.com>
2024-10-28 23:50:44 +0900

    feat(FE): 카운팅 애니메이션 훅 제작

--- front/src/shared/hooks/index.ts
+++ front/src/shared/hooks/index.ts
@@ -1,5 +1,11 @@
 import useMediaQuery from "./useMediaQuery";
 import useScrollDirection from "./useScrollDirection";
 import useIntersectionObserver from "./useIntersectionObserver";
+import { useCountingAnimation } from "./useCountingAnimation";
 
-export { useMediaQuery, useScrollDirection, useIntersectionObserver };
+export {
+  useMediaQuery,
+  useScrollDirection,
+  useIntersectionObserver,
+  useCountingAnimation,
+};
---
+++ front/src/shared/hooks/useCountingAnimation.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { useAnimation } from "framer-motion";
+
+/**
+ * 카운팅 애니메이션 훅
+ * @param initialCount 초기 카운트 값
+ * @returns 현재 카운트, 현재 카운트 설정 함수, 카운트 애니메이션 컨트롤
+ */
+export function useCountingAnimation(initialCount: number) {
+  const [currentCount, setCurrentCount] = useState(initialCount);
+  const [previousCount, setPreviousCount] = useState(initialCount);
+  const countControls = useAnimation();
+
+  useEffect(() => {
+    countControls.start(
+      currentCount > previousCount
+        ? {
+            opacity: [0, 1],
+            y: [10, 0],
+            transition: { duration: 0.3 },
+          }
+        : {
+            opacity: [0, 1],
+            y: [-10, 0],
+            transition: { duration: 0.3 },
+          }
+    );
+
+    setPreviousCount(currentCount);
+  }, [currentCount]);
+
+  return [currentCount, setCurrentCount, countControls] as const;
+}
