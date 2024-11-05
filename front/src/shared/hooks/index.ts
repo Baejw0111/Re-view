aea5b0cb9ebd184d27ebf549ca31ca4f70c48d6e
Baejw0111 <bjw0111@naver.com>
2024-11-05 17:48:39 +0900

    feat(FE): Tailwind Breakpoint Hook 추가
    - Tailwind CSS의 반응형 디자인을 지원하기 위한 useTailwindBreakpoint 훅 추가
    - 화면 크기에 따라 현재 브레이크포인트를 감지하여 상태로 관리
    - 리사이즈 이벤트를 통해 브레이크포인트 업데이트

--- front/src/shared/hooks/index.ts
+++ front/src/shared/hooks/index.ts
@@ -2,10 +2,12 @@ import useMediaQuery from "./useMediaQuery";
 import useScrollDirection from "./useScrollDirection";
 import useIntersectionObserver from "./useIntersectionObserver";
 import useCountingAnimation from "./useCountingAnimation";
+import useTailwindBreakpoint from "./useTailwindBreakpoint";
 
 export {
   useMediaQuery,
   useScrollDirection,
   useIntersectionObserver,
   useCountingAnimation,
+  useTailwindBreakpoint,
 };
