b96c8d4622d7278e8b0ac13da0370c9a6ad6d7d9
Baejw0111 <bjw0111@naver.com>
2024-11-05 17:54:36 +0900

    feat(FE): 리뷰 목록에 가상 스크롤 및 반응형 그리드 추가
    - `useWindowVirtualizer`를 사용하여 가상 스크롤 기능을 추가하여 성능 최적화
    - `useTailwindBreakpoint`를 사용하여 반응형 그리드 칼럼 수를 동적으로 설정

--- front/src/widgets/Reviews.tsx
+++ front/src/widgets/Reviews.tsx
@@ -1,5 +1,9 @@
+import { useRef } from "react";
 import ReviewCard from "@/widgets/ReviewCard";
 import { useIntersectionObserver } from "@/shared/hooks";
+import { useWindowVirtualizer } from "@tanstack/react-virtual";
+import { VirtualItem } from "@tanstack/react-virtual";
+import { useTailwindBreakpoint } from "@/shared/hooks";
 
 /**
  * @description 리뷰 카드 목록을 반환하는 컴포넌트
@@ -13,23 +17,83 @@ export default function Reviews({
   reviewIdList: string[];
   callback: () => void;
 }) {
-  const reviewListRef = useIntersectionObserver<string>({
+  const breakpoint = useTailwindBreakpoint(); // 현재 화면 너비
+  const listRef = useRef<HTMLDivElement>(null);
+  const gridColumnCount = {
+    xs: 1,
+    sm: 2,
+    md: 2,
+    lg: 3,
+    xl: 3,
+    "2xl": 4,
+  }; // 현재 화면 너비에 따른 그리드 칼럼 수
+
+  const virtualizer = useWindowVirtualizer({
+    count: Math.ceil(reviewIdList.length / gridColumnCount[breakpoint]),
+    estimateSize: () => 240,
+    gap: 24,
+    overscan: 2,
+  });
+
+  const infiniteScrollRef = useIntersectionObserver<VirtualItem>({
     callback,
-    list: reviewIdList,
+    dependency: virtualizer.getVirtualItems(),
   });
 
   return (
     <>
       {reviewIdList && (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6">
-          {reviewIdList.map((reviewId, index) => (
-            <div
-              key={reviewId}
-              ref={index === reviewIdList.length - 5 ? reviewListRef : null}
-            >
-              <ReviewCard reviewId={reviewId} />
-            </div>
-          ))}
+        <div ref={listRef}>
+          <div
+            className="relative w-full"
+            style={{
+              height: `${virtualizer.getTotalSize()}px`,
+            }}
+          >
+            {virtualizer.getVirtualItems().map((item) => (
+              <div
+                key={reviewIdList[item.index]}
+                className="absolute top-0 left-0 w-full"
+                data-index={item.index}
+                ref={virtualizer.measureElement}
+                style={{
+                  transform: `translateY(${
+                    item.start - virtualizer.options.scrollMargin
+                  }px)`,
+                }}
+              >
+                <div
+                  className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6"
+                  ref={
+                    item.index ===
+                    Math.ceil(
+                      reviewIdList.length / gridColumnCount[breakpoint]
+                    ) -
+                      2
+                      ? infiniteScrollRef
+                      : null
+                  }
+                >
+                  {Array.from({ length: gridColumnCount[breakpoint] }).map(
+                    (_, index) => (
+                      <ReviewCard
+                        key={
+                          reviewIdList[
+                            item.index * gridColumnCount[breakpoint] + index
+                          ]
+                        }
+                        reviewId={
+                          reviewIdList[
+                            item.index * gridColumnCount[breakpoint] + index
+                          ]
+                        }
+                      />
+                    )
+                  )}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       )}
     </>
