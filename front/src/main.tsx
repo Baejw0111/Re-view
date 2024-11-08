53f7df6155304e1e3b7ee70ebad591f44e58827a
Baejw0111 <bjw0111@naver.com>
2024-11-08 15:25:44 +0900

    feat(FE): `main.tsx`에 QueryClient 기본 옵션 추가
    - QueryClient에 기본 옵션 설정 추가
    - 쿼리의 staleTime을 5분으로 설정하여 데이터 캐싱 전략 개선

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -6,7 +6,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import store from "@/state/store";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
