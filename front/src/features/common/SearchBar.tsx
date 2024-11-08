7c71476c6ba13f5e5d9eca512fac7148c9b1e5a9
Baejw0111 <bjw0111@naver.com>
2024-11-09 02:33:13 +0900

    feat(FE): SearchBar 컴포넌트에 태그 검색 기능 구현
    - 인기 태그와 연관 태그 검색 기능 추가
    - 검색어 입력에 따른 실시간 연관 태그 조회 구현
    - 검색창 열 때 인기 태그 자동 로드 기능 추가

--- front/src/features/common/SearchBar.tsx
+++ front/src/features/common/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/shared/shadcn-ui/button";
 import { Search } from "lucide-react";
 import {
@@ -10,16 +10,44 @@ import {
   CommandItem,
   CommandSeparator,
 } from "@/shared/shadcn-ui/command";
+import { useQuery } from "@tanstack/react-query";
+import { fetchPopularTags, fetchSearchRelatedTags } from "@/api/tag";
 
 export default function SearchBar() {
   const [open, setOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  // 인기 태그 불러오기
+  const { data: popularTags, refetch: refetchPopularTags } = useQuery({
+    queryKey: ["popularTags"],
+    queryFn: fetchPopularTags,
+  });
+  // 최근 검색어 불러오기 및 저장 기능 필요
+
+  // 연관 태그 불러오기
+  const { data: searchRelatedTags, refetch: refetchSearchRelatedTags } =
+    useQuery({
+      queryKey: ["searchRelatedTags"],
+      queryFn: () => fetchSearchRelatedTags(searchQuery),
+      enabled: !!searchQuery,
+    });
+
+  const onOpenSearchDialog = () => {
+    setOpen(true);
+    refetchPopularTags();
+  };
+
+  useEffect(() => {
+    if (searchQuery) {
+      refetchSearchRelatedTags();
+    }
+  }, [searchQuery]);
 
   return (
     <>
       <Button
         variant="secondary"
         className="h-9 w-full md:w-60 border border-foreground/10"
-        onClick={() => setOpen(true)}
+        onClick={onOpenSearchDialog}
       >
         <div className="w-full flex items-center gap-2 text-foreground/50">
           <Search className="h-5 w-5" />
@@ -27,18 +55,40 @@ export default function SearchBar() {
         </div>
       </Button>
       <CommandDialog open={open} onOpenChange={setOpen}>
-        <CommandInput placeholder="검색어를 입력해주세요." />
+        <CommandInput
+          placeholder="검색어를 입력해주세요."
+          value={searchQuery}
+          onValueChange={(value) => setSearchQuery(value)}
+        />
         <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="연관 태그"></CommandGroup>
-          <CommandSeparator />
-          <CommandGroup heading="인기 태그"></CommandGroup>
-          <CommandSeparator />
-          <CommandGroup heading="최근 검색어">
-            <CommandItem>
-              <span>Calendar</span>
-            </CommandItem>
-          </CommandGroup>
+          {searchQuery && (
+            <>
+              <CommandGroup heading="연관 태그">
+                {searchRelatedTags &&
+                  searchRelatedTags.map((tag: string) => (
+                    <CommandItem key={tag}>{tag}</CommandItem>
+                  ))}
+              </CommandGroup>
+              <CommandSeparator />
+            </>
+          )}
+          <CommandEmpty>"검색어와 관련된 태그가 없습니다."</CommandEmpty>
+          {!searchQuery && (
+            <>
+              <CommandGroup heading="최근 검색어">
+                <CommandItem>
+                  <span>Calendar</span>
+                </CommandItem>
+              </CommandGroup>
+              <CommandSeparator />
+              <CommandGroup heading="인기 태그">
+                {popularTags &&
+                  popularTags.map((tag: string) => (
+                    <CommandItem key={tag}>{tag}</CommandItem>
+                  ))}
+              </CommandGroup>
+            </>
+          )}
         </CommandList>
       </CommandDialog>
     </>
