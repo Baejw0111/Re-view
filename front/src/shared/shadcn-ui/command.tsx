e008df1992ea5ef28219e2114d20e6bf9ec1c8a2
Baejw0111 <bjw0111@naver.com>
2024-11-12 13:42:46 +0900

    feat(FE): Command 컴포넌트에 한글 초성 검색 기능 추가
    - 검색 필터링 로직을 추가하여 한글 초성 검색 지원
    - 공백을 제거한 검색도 가능하도록 개선
    - es-hangul 라이브러리를 활용한 초성 추출 기능 구현

--- front/src/shared/shadcn-ui/command.tsx
+++ front/src/shared/shadcn-ui/command.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { type DialogProps } from "@radix-ui/react-dialog";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { Command as CommandPrimitive } from "cmdk";
+import { filterCommand } from "@/shared/lib/utils";
 
 import { cn } from "@/shared/lib/utils";
 import {
@@ -35,7 +36,7 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
         <DialogTitle hidden />
         <DialogDescription hidden />
         <Command
-          shouldFilter={false}
+          filter={filterCommand}
           loop
           className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5"
         >
