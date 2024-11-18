b692d4ccd23cdf882f490287c3b0d84ca9e2a457
Baejw0111 <bjw0111@naver.com>
2024-11-18 20:24:01 +0900

    fix(FE): `useAuth` 훅에서 불필요한 리패치 방지
    - `refetchOnWindowFocus` 옵션을 false로 설정하여 창 포커스 시 불필요한 리패치를 방지함
    - 사용자 경험을 개선하고 불필요한 네트워크 요청을 줄임

--- front/src/shared/hooks/useAuth.tsx
+++ front/src/shared/hooks/useAuth.tsx
@@ -5,6 +5,7 @@ import { API_URL } from "@/shared/constants";
 export default function useAuth() {
   const { isError, isPending } = useQuery({
     queryKey: ["auth"],
+    refetchOnWindowFocus: false,
     retry: false,
     queryFn: async () =>
       await axios.get(`${API_URL}/auth/kakao/check`, {
