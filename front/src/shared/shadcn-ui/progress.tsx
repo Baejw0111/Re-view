a3b35bb07d90e64a25877ff7aefe33aabad96747
Baejw0111 <bjw0111@naver.com>
2024-11-26 02:34:01 +0900

    chore(FE): shadcn/ui의 Progress 컴포넌트 설치 및 커스텀
    - 바 중앙에 값을 나타내는 텍스트 표시하도록 추가적으로 커스텀
    - 진행 바 색을 `blue-500`으로 커스텀

---
+++ front/src/shared/shadcn-ui/progress.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+
+import { cn } from "@/shared/lib/utils";
+
+const Progress = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+>(({ className, value, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative h-3 w-full overflow-hidden rounded-full bg-primary/20",
+      className
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className="h-full w-full flex-1 bg-primary transition-all bg-blue-500"
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    />
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <p className="text-xs font-medium transition-all duration-300">
+        {value}%
+      </p>
+    </div>
+  </ProgressPrimitive.Root>
+));
+Progress.displayName = ProgressPrimitive.Root.displayName;
+
+export { Progress };
