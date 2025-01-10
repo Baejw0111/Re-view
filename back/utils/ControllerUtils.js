c85a8c97d7c6f59e51eb26b634311fa3cc7e50b3
Baejw0111 <bjw0111@naver.com>
2025-01-11 01:17:02 +0900

    refactor(BE): 개선된 에러 핸들링 및 JSON 응답 로깅 추가
    - handleError 함수에서 에러 메시지를 간소화하여 클라이언트에 전달
    - asyncHandler에서 400-499 상태 코드의 JSON 응답을 로깅하는 기능 추가

--- back/utils/ControllerUtils.js
+++ back/utils/ControllerUtils.js
@@ -17,9 +17,7 @@ const handleError = (req, res, operation) => (error) => {
     deleteUploadedFiles(req.files.map((file) => file.key));
   }
 
-  res
-    .status(500)
-    .json({ message: "서버 에러가 발생했습니다.", error: error.message });
+  res.status(500).json({ message: `서버 에러: ${error.message}` });
 };
 
 /**
@@ -30,6 +28,17 @@ const handleError = (req, res, operation) => (error) => {
  */
 const asyncHandler = (fn, operation) => (req, res, next) => {
   console.log(`\x1b[32m실행 작업: ${operation}\x1b[0m`);
+
+  // 400번대 에러 처리
+  const originalSend = res.json; // 원래 함수 저장
+  res.json = function (body) {
+    if (res.statusCode >= 400 && res.statusCode < 500) {
+      console.error(
+        `\x1b[31m${operation} 작업 중 ${res.statusCode} 에러 발생: ${body.message}\x1b[0m`
+      );
+    }
+    return originalSend.apply(this, arguments); // 원래 함수 실행
+  };
 
   return Promise.resolve(fn(req, res, next)).catch(
     handleError(req, res, operation)
