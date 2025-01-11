1bbf85499b512db32cae9a6a8deaa93d2ccd0e15
Baejw0111 <bjw0111@naver.com>
2025-01-11 13:42:59 +0900

    refactor(FE): SubHeader 컴포넌트의 구조 개선 및 불필요한 JSX 제거
    - SubHeader 컴포넌트에서 불필요한 Fragment 제거 및 JSX 구조를 간소화하여 가독성 향상
    - 조건부 렌더링을 통해 불필요한 DOM 요소를 줄이고, 코드의 일관성을 유지

--- front/src/widgets/SubHeader.tsx
+++ front/src/widgets/SubHeader.tsx
@@ -28,10 +28,8 @@ export default function SubHeader() {
   if (!routeMap[pageRoute as keyof typeof routeMap]) return null;
 
   return (
-    <>
-      {window.location.pathname !== "/oauth/kakao" && (
-        <div
-          className={`sticky left-0 z-40 w-full backdrop-blur-xl supports-[backdrop-filter]:bg-primary-foreground/40 transition-all duration-300 ease-in-out
+    <div
+      className={`sticky left-0 z-40 w-full backdrop-blur-xl supports-[backdrop-filter]:bg-primary-foreground/40 transition-all duration-300 ease-in-out
             ${isMobile ? "top-0" : "top-16"}
             ${
               isScrollingUp
@@ -41,98 +39,94 @@ export default function SubHeader() {
                 : "-translate-y-16"
             }
           `}
-        >
-          <div className="flex items-center h-14 md:h-16 justify-between max-w-screen-2xl mx-auto px-4 md:px-6 py-2 md:py-4 border-b border-border">
-            <h1 className="text-2xl md:text-3xl font-bold line-clamp-1 mr-4">
-              {pageRoute === "search" ? (
-                <div className="flex items-center">
-                  <Search className="h-6 w-6 md:h-8 md:w-8 shrink-0 mr-2" />
-                  <span className="line-clamp-1">"{searchQuery}</span>
-                  <span>"</span>
-                </div>
-              ) : (
-                `${routeMap[pageRoute as keyof typeof routeMap]}`
-              )}
-            </h1>
-            {routeMap[pageRoute as keyof typeof routeMap] === "피드" && (
-              <Tabs
-                defaultValue="latest"
-                value={
-                  pathname.split("/")[1] === "popular" ? "popular" : "latest"
-                }
-              >
-                <TabsList>
-                  <TabsTrigger value="latest" asChild>
-                    <Link
-                      to="/latest"
-                      onClick={() =>
-                        queryClient.invalidateQueries({
-                          queryKey: ["feed", "latest"],
-                        })
-                      }
-                    >
-                      최신글
-                    </Link>
-                  </TabsTrigger>
-                  <TabsTrigger value="popular" asChild>
-                    <Link
-                      to="/popular"
-                      onClick={() =>
-                        queryClient.invalidateQueries({
-                          queryKey: ["feed", "popular"],
-                        })
-                      }
-                    >
-                      인기글
-                    </Link>
-                  </TabsTrigger>
-                </TabsList>
-              </Tabs>
-            )}
-            {routeMap[pageRoute as keyof typeof routeMap] === "검색 결과" && (
-              <Tabs
-                defaultValue="reviews"
-                value={
-                  queryParams.get("category") === "users" ? "users" : "reviews"
-                }
-              >
-                <TabsList>
-                  <TabsTrigger value="reviews" asChild>
-                    <Link
-                      to={`?${new URLSearchParams({
-                        ...Object.fromEntries(queryParams),
-                        category: "reviews",
-                      })}`}
-                      onClick={() =>
-                        queryClient.invalidateQueries({
-                          queryKey: ["search", "reviews"],
-                        })
-                      }
-                    >
-                      리뷰
-                    </Link>
-                  </TabsTrigger>
-                  <TabsTrigger value="users" asChild>
-                    <Link
-                      to={`?${new URLSearchParams({
-                        ...Object.fromEntries(queryParams),
-                        category: "users",
-                      })}`}
-                      onClick={() =>
-                        queryClient.invalidateQueries({
-                          queryKey: ["search", "users"],
-                        })
-                      }
-                    >
-                      유저
-                    </Link>
-                  </TabsTrigger>
-                </TabsList>
-              </Tabs>
-            )}
-          </div>
-        </div>
-      )}
-    </>
+    >
+      <div className="flex items-center h-14 md:h-16 justify-between max-w-screen-2xl mx-auto px-4 md:px-6 py-2 md:py-4 border-b border-border">
+        <h1 className="text-2xl md:text-3xl font-bold line-clamp-1 mr-4">
+          {pageRoute === "search" ? (
+            <div className="flex items-center">
+              <Search className="h-6 w-6 md:h-8 md:w-8 shrink-0 mr-2" />
+              <span className="line-clamp-1">"{searchQuery}</span>
+              <span>"</span>
+            </div>
+          ) : (
+            `${routeMap[pageRoute as keyof typeof routeMap]}`
+          )}
+        </h1>
+        {routeMap[pageRoute as keyof typeof routeMap] === "피드" && (
+          <Tabs
+            defaultValue="latest"
+            value={pathname.split("/")[1] === "popular" ? "popular" : "latest"}
+          >
+            <TabsList>
+              <TabsTrigger value="latest" asChild>
+                <Link
+                  to="/latest"
+                  onClick={() =>
+                    queryClient.invalidateQueries({
+                      queryKey: ["feed", "latest"],
+                    })
+                  }
+                >
+                  최신글
+                </Link>
+              </TabsTrigger>
+              <TabsTrigger value="popular" asChild>
+                <Link
+                  to="/popular"
+                  onClick={() =>
+                    queryClient.invalidateQueries({
+                      queryKey: ["feed", "popular"],
+                    })
+                  }
+                >
+                  인기글
+                </Link>
+              </TabsTrigger>
+            </TabsList>
+          </Tabs>
+        )}
+        {routeMap[pageRoute as keyof typeof routeMap] === "검색 결과" && (
+          <Tabs
+            defaultValue="reviews"
+            value={
+              queryParams.get("category") === "users" ? "users" : "reviews"
+            }
+          >
+            <TabsList>
+              <TabsTrigger value="reviews" asChild>
+                <Link
+                  to={`?${new URLSearchParams({
+                    ...Object.fromEntries(queryParams),
+                    category: "reviews",
+                  })}`}
+                  onClick={() =>
+                    queryClient.invalidateQueries({
+                      queryKey: ["search", "reviews"],
+                    })
+                  }
+                >
+                  리뷰
+                </Link>
+              </TabsTrigger>
+              <TabsTrigger value="users" asChild>
+                <Link
+                  to={`?${new URLSearchParams({
+                    ...Object.fromEntries(queryParams),
+                    category: "users",
+                  })}`}
+                  onClick={() =>
+                    queryClient.invalidateQueries({
+                      queryKey: ["search", "users"],
+                    })
+                  }
+                >
+                  유저
+                </Link>
+              </TabsTrigger>
+            </TabsList>
+          </Tabs>
+        )}
+      </div>
+    </div>
   );
 }
