288337315a74e0bb5ff2f1ef0cb4de44800d8891
Baejw0111 <bjw0111@naver.com>
2025-01-15 04:03:23 +0900

    feat(FE): 사용자 프로필 편집을 위한 닉네임 및 프로필 이미지 폼 추가
    - 사용자 프로필 편집을 간소화하기 위해 `NicknameForm`과 `ProfileImageForm` 컴포넌트를 도입
    - 코드 조직과 가독성을 향상시키기 위해 `EditUserProfile` 컴포넌트를 리팩토링하여 새로운 폼을 활용
    - 회원가입 시 사용자 경험을 개선하기 위해 온보딩 과정에 프로필 이미지와 닉네임 입력 컴포넌트 사용

--- front/src/features/setting/EditUserProfile.tsx
+++ front/src/features/setting/EditUserProfile.tsx
@@ -1,59 +1,36 @@
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useSelector } from "react-redux";
-import { Input } from "@/shared/shadcn-ui/input";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/shared/shadcn-ui/form";
+import { Form } from "@/shared/shadcn-ui/form";
 import { RootState } from "@/state/store";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation } from "@tanstack/react-query";
 import { updateUserInfo } from "@/api/user";
-import { signUp } from "@/api/auth";
-import { Avatar, AvatarFallback, AvatarImage } from "@/shared/shadcn-ui/avatar";
-import { Camera, LoaderCircle, UserRound, X } from "lucide-react";
-import UserAvatar from "@/features/user/UserAvatar";
-import {
-  handleEnterKeyDown,
-  createPreviewImages,
-  convertToWebP,
-  resetFileInput,
-  revokePreviewImages,
-} from "@/shared/lib/utils";
+import { handleEnterKeyDown } from "@/shared/lib/utils";
 import {
   profileImageValidation,
   nicknameValidation,
-  useDefaultProfileValidation,
-  reviewFieldLimits,
 } from "@/shared/types/validation";
-import { Skeleton } from "@/shared/shadcn-ui/skeleton";
+import ProfileImageForm from "../form/ProfileImageForm";
+import NicknameForm from "../form/NicknameForm";
 
 export default function EditUserProfile({
   submitFooter,
-  isOnboarding = false,
 }: {
   submitFooter: (isUploading: boolean) => React.ReactNode;
-  isOnboarding?: boolean;
 }) {
   const userInfo = useSelector((state: RootState) => state.userInfo); // 사용자 정보
-  const [currentProfileImage, setCurrentProfileImage] = useState<string>(""); // 사용자가 현재 등록한 프로필 이미지
   const [isUploading, setIsUploading] = useState(false); // 프로필 이미지 업로드 상태
-  const [uploadProgress, setUploadProgress] = useState(0); // 프로필 이미지 업로드 진행률
 
   const formSchema = z.object({
     profileImage: profileImageValidation(),
     newNickname: nicknameValidation,
-    useDefaultProfile: useDefaultProfileValidation,
+    useDefaultProfile: z.boolean(),
   });
 
   /**
-   * 프로필 업데이트 폼
+   * 프로필 업데이트 폼 정의
    */
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -68,13 +45,9 @@ export default function EditUserProfile({
 
   // 프로필 업데이트
   const { mutate: updateUserInfoMutation } = useMutation({
-    mutationFn: isOnboarding ? signUp : updateUserInfo,
+    mutationFn: updateUserInfo,
     onSuccess: () => {
-      if (isOnboarding) {
-        window.location.href = "/";
-      } else {
-        window.location.reload();
-      }
+      window.location.reload();
     },
   });
 
@@ -91,59 +64,16 @@ export default function EditUserProfile({
     updateUserInfoMutation(formData);
   };
 
-  /**
-   * 프로필 이미지 업로드 핸들러
-   * @param e 프로필 이미지 업로드 이벤트
-   */
-  const handleProfileImageUpload = async (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setIsUploading(true);
-    setUploadProgress(0);
-    const newFiles = e.target.files;
-
-    if (newFiles) {
-      const webpFiles = await convertToWebP(newFiles, (progress) => {
-        setUploadProgress(progress);
-      }); // 파일을 webp로 변환
-      const previewImage = await createPreviewImages(webpFiles); // 미리보기 이미지 생성
-      setCurrentProfileImage(previewImage[0]); // 현재 프로필 이미지 업데이트
-      form.setValue("profileImage", webpFiles); // 폼 값 업데이트
-      form.setValue("useDefaultProfile", false); // 기본 프로필 이미지 사용 여부 업데이트
-    }
-    setIsUploading(false);
-  };
-
-  /**
-   * 기본 프로필 이미지 핸들러
-   */
-  const handleDefaultProfileImage = () => {
-    setCurrentProfileImage("");
-    form.setValue("useDefaultProfile", true);
-    form.setValue("profileImage", new DataTransfer().files);
-
-    resetFileInput("profileImage-upload");
-  };
-
   // 현재 프로필 정보 폼에 적용
   useEffect(() => {
-    if (userInfo.isSignedUp) {
-      form.setValue("profileImage", new DataTransfer().files);
-      form.setValue("newNickname", userInfo.nickname);
-      if (userInfo.profileImage.length > 0) {
-        form.setValue("useDefaultProfile", false);
-      } else {
-        form.setValue("useDefaultProfile", true);
-      }
+    form.setValue("profileImage", new DataTransfer().files);
+    form.setValue("newNickname", userInfo.nickname);
+    if (userInfo.profileImage.length > 0) {
+      form.setValue("useDefaultProfile", false);
+    } else {
+      form.setValue("useDefaultProfile", true);
     }
   }, [userInfo, form]);
-
-  useEffect(() => {
-    // 컴포넌트 언마운트 시 메모리 해제
-    return () => {
-      revokePreviewImages([currentProfileImage]);
-    };
-  }, [currentProfileImage]);
 
   return (
     <Form {...form}>
@@ -152,96 +82,17 @@ export default function EditUserProfile({
         onKeyDown={handleEnterKeyDown}
         className="flex flex-col gap-4"
       >
-        <FormField
-          control={form.control}
-          name="profileImage"
-          render={() => (
-            <>
-              <FormItem>
-                <FormControl>
-                  <div className="flex flex-col items-center gap-4">
-                    <div className="relative">
-                      <FormLabel
-                        htmlFor="profileImage-upload"
-                        className="cursor-pointer"
-                      >
-                        {currentProfileImage.length > 0 || isDefaultProfile ? (
-                          <Avatar className="h-24 w-24 transition-transform hover:scale-105 active:scale-105">
-                            {isUploading ? (
-                              <Skeleton className="h-24 w-24 flex items-center justify-center">
-                                <LoaderCircle className="w-6 h-6 animate-spin" />
-                              </Skeleton>
-                            ) : (
-                              <>
-                                <AvatarImage
-                                  src={currentProfileImage}
-                                  alt={userInfo.nickname}
-                                  className="object-cover"
-                                />
-                                <AvatarFallback>
-                                  <UserRound className="w-[70%] h-[70%]" />
-                                </AvatarFallback>
-                              </>
-                            )}
-                          </Avatar>
-                        ) : (
-                          <UserAvatar
-                            className="h-24 w-24 transition-transform hover:scale-105 active:scale-105"
-                            profileImage={userInfo.profileImage}
-                            nickname={userInfo.nickname}
-                          />
-                        )}
-                        <div className="absolute bottom-0 right-0 bg-muted-foreground rounded-full p-1 shadow-lg">
-                          <Camera className="w-4 h-4 text-white" />
-                        </div>
-                      </FormLabel>
-                      <input
-                        id="profileImage-upload"
-                        type="file"
-                        accept={reviewFieldLimits.imageTypes.join(", ")}
-                        className="hidden"
-                        onChange={handleProfileImageUpload}
-                      />
-                      {!isDefaultProfile && (
-                        <button
-                          type="button"
-                          onClick={handleDefaultProfileImage}
-                          className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full p-1 shadow-lg hover:bg-red-800 active:bg-red-800 transition-colors"
-                          aria-label="프로필 사진 삭제"
-                        >
-                          <X className="w-4 h-4" />
-                        </button>
-                      )}
-                    </div>
-                    <span className="text-sm text-muted-foreground">
-                      {isUploading
-                        ? `이미지 업로드 중... ${uploadProgress}%`
-                        : "클릭하여 프로필 사진 업로드"}
-                    </span>
-                  </div>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </>
-          )}
+        {/* 프로필 이미지 */}
+        <ProfileImageForm
+          form={form}
+          isDefaultProfile={isDefaultProfile}
+          isUploading={isUploading}
+          setIsUploading={setIsUploading}
+          userInfo={userInfo}
         />
-        <FormField
-          control={form.control}
-          name="newNickname"
-          render={({ field }) => (
-            <>
-              <FormItem>
-                <FormControl>
-                  <div className="flex flex-col gap-2">
-                    <div className="text-sm font-bold">닉네임</div>
-                    <Input id="newNickname" {...field} />
-                  </div>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </>
-          )}
-        />
+
+        {/* 닉네임 */}
+        <NicknameForm form={form} />
         {submitFooter(isUploading)}
       </form>
     </Form>
--- front/src/pages/Onboarding.tsx
+++ front/src/pages/Onboarding.tsx
@@ -1,7 +1,16 @@
 import { useState, useEffect } from "react";
+import { Form } from "@/shared/shadcn-ui/form";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import {
+  profileImageValidation,
+  nicknameValidation,
+} from "@/shared/types/validation";
+import { handleEnterKeyDown } from "@/shared/lib/utils";
 import { Button } from "@/shared/shadcn-ui/button";
 import { motion } from "framer-motion";
-import EditUserProfile from "@/features/setting/EditUserProfile";
 import {
   Card,
   CardContent,
@@ -10,6 +19,8 @@ import {
   CardHeader,
 } from "@/shared/shadcn-ui/card";
 import MovingLogo from "@/features/common/MovingLogo";
+import ProfileImageForm from "@/features/form/ProfileImageForm";
+import NicknameForm from "@/features/form/NicknameForm";
 import { Checkbox } from "@/shared/shadcn-ui/checkbox";
 import { Label } from "@/shared/shadcn-ui/label";
 import { useNavigate } from "react-router-dom";
@@ -17,11 +28,14 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/state/store";
 import TermsDialog from "@/widgets/TermsDialog";
 import PrivacyAgreementDialog from "@/widgets/PrivacyAgreementDialog";
+import { signUp } from "@/api/auth";
 
 export default function Onboarding() {
   const navigate = useNavigate();
-  const [isAgreementChecked, setIsAgreementChecked] = useState(false);
   const userInfo = useSelector((state: RootState) => state.userInfo);
+  const [isAgreementChecked, setIsAgreementChecked] = useState(false);
+  const [isUploading, setIsUploading] = useState(false); // 프로필 이미지 업로드 상태
+
   const containerVariants = {
     hidden: { opacity: 0 },
     visible: {
@@ -45,7 +59,52 @@ export default function Onboarding() {
     },
   };
 
+  /**
+   * 회원 가입 폼 스키마 정의
+   */
+  const formSchema = z.object({
+    profileImage: profileImageValidation(),
+    newNickname: nicknameValidation,
+    useDefaultProfile: z.boolean(),
+  });
+
+  /**
+   * 프로필 업데이트 폼 정의
+   */
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      profileImage: new DataTransfer().files,
+      newNickname: "",
+      useDefaultProfile: true,
+    },
+  });
+
+  const isDefaultProfile = form.watch("useDefaultProfile"); // 기본 프로필 이미지 사용 여부
+
+  // 프로필 업데이트
+  const { mutate: signUpMutation } = useMutation({
+    mutationFn: signUp,
+    onSuccess: () => {
+      window.location.href = "/";
+    },
+  });
+
+  /**
+   * 프로필 업데이트 제출 함수
+   * @param values 프로필 업데이트 제출 값
+   */
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    const { profileImage, newNickname, useDefaultProfile } = values;
+    const formData = new FormData();
+    formData.append("profileImage", profileImage[0]);
+    formData.append("newNickname", newNickname);
+    formData.append("useDefaultProfile", useDefaultProfile.toString());
+    signUpMutation(formData);
+  };
+
   useEffect(() => {
+    // 이미 회원가입 된 사용자가 Onboarding 페이지에 접속한 경우 메인 페이지로 이동
     if (userInfo?.isSignedUp) {
       navigate("/");
     }
@@ -72,47 +131,67 @@ export default function Onboarding() {
             </motion.span>
           </CardDescription>
 
-          <CardContent>
-            <motion.div variants={itemVariants}>
-              <EditUserProfile
-                isOnboarding
-                submitFooter={(isUploading: boolean) => (
-                  <CardFooter className="flex flex-col items-center gap-2 p-0">
-                    <motion.div
-                      variants={itemVariants}
-                      className="flex flex-col items-center gap-2 w-full my-2"
-                    >
-                      <div className="flex items-center gap-2 w-full">
-                        <Checkbox
-                          id="agreement"
-                          checked={isAgreementChecked}
-                          onCheckedChange={() =>
-                            setIsAgreementChecked(!isAgreementChecked)
-                          }
-                        />
-                        <Label htmlFor="agreement" className="font-medium">
-                          필수 약관에 동의합니다
-                        </Label>
-                      </div>
-                      <div className="w-full text-sm text-muted-foreground">
-                        <TermsDialog />및
-                        <PrivacyAgreementDialog />에 동의합니다.
-                      </div>
-                    </motion.div>
-                    <motion.div variants={itemVariants} className="w-full">
-                      <Button
-                        type="submit"
-                        variant="outline"
-                        className="w-full relative overflow-hidden group"
-                        disabled={isUploading || !isAgreementChecked}
-                      >
-                        <span className="relative z-10">회원 가입 완료</span>
-                        <span
-                          className="absolute inset-0 bg-gradient-to-r from-red-500 via-green-500 to-blue-500
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              onKeyDown={handleEnterKeyDown}
+              className="flex flex-col gap-4"
+            >
+              <CardContent>
+                <motion.div variants={itemVariants}>
+                  {/* 프로필 이미지 */}
+                  <ProfileImageForm
+                    form={form}
+                    isDefaultProfile={isDefaultProfile}
+                    isUploading={isUploading}
+                    setIsUploading={setIsUploading}
+                    userInfo={userInfo}
+                  />
+                </motion.div>
+
+                <motion.div variants={itemVariants}>
+                  {/* 닉네임 */}
+                  <NicknameForm form={form} />
+                </motion.div>
+
+                <motion.div
+                  variants={itemVariants}
+                  className="flex flex-col items-center gap-2 w-full my-2"
+                >
+                  <div className="flex items-center gap-2 w-full">
+                    <Checkbox
+                      id="agreement"
+                      checked={isAgreementChecked}
+                      onCheckedChange={() =>
+                        setIsAgreementChecked(!isAgreementChecked)
+                      }
+                    />
+                    <Label htmlFor="agreement" className="font-medium">
+                      필수 약관에 동의합니다
+                    </Label>
+                  </div>
+                  <div className="w-full text-sm text-muted-foreground">
+                    <TermsDialog />및
+                    <PrivacyAgreementDialog />에 동의합니다.
+                  </div>
+                </motion.div>
+              </CardContent>
+
+              <motion.div variants={itemVariants} className="w-full">
+                <CardFooter>
+                  <Button
+                    type="submit"
+                    variant="outline"
+                    className="w-full relative overflow-hidden group"
+                    disabled={isUploading || !isAgreementChecked}
+                  >
+                    <span className="relative z-10">회원 가입 하기</span>
+                    <span
+                      className="absolute inset-0 bg-gradient-to-r from-red-500 via-green-500 to-blue-500
                       opacity-0 group-hover:opacity-100 group-active:opacity-100 group-focus:opacity-100 animate-shimmer transition-opacity duration-300"
-                        ></span>
-                      </Button>
-                      <style>{`
+                    ></span>
+                  </Button>
+                  <style>{`
                       @keyframes shimmer {
                         0% { background-position: 200% 0; }
                         100% { background-position: -200% 0; }
@@ -122,12 +201,10 @@ export default function Onboarding() {
                         animation: shimmer 0.5s linear infinite;
                       }
                     `}</style>
-                    </motion.div>
-                  </CardFooter>
-                )}
-              />
-            </motion.div>
-          </CardContent>
+                </CardFooter>
+              </motion.div>
+            </form>
+          </Form>
         </Card>
       </motion.div>
     </div>
