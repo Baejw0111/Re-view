eb0bbadcac417aa8393428ffd547962dd0cf24ee
Baejw0111 <bjw0111@naver.com>
2025-01-16 13:22:53 +0900

    refactor(FE): ProfileButton 컴포넌트 개선 및 불필요한 코드 제거
    - 사용자 피드백 제출 기능 및 관련 다이얼로그 제거
    - 프로필 편집 버튼을 설정 버튼으로 변경하여 설정 페이지로 이동하도록 수정
    - 로그아웃 기능을 useMutation을 통해 처리하도록 개선
    - 불필요한 import 및 상태 관리 코드 정리

--- front/src/features/user/ProfileButton.tsx
+++ front/src/features/user/ProfileButton.tsx
@@ -1,40 +1,18 @@
-import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
 import { Button } from "@/shared/shadcn-ui/button";
 import TooltipWrapper from "@/shared/original-ui/TooltipWrapper";
 import UserAvatar from "@/features/user/UserAvatar";
 import { LoginUserInfo } from "@/shared/types/interface";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useTheme } from "@/state/theme/useTheme";
-import { useMutation } from "@tanstack/react-query";
-import { toast } from "sonner";
-import { sendUserFeedback } from "@/api/user";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/shared/shadcn-ui/dropdown-menu";
-import {
-  Moon,
-  Sun,
-  UserRound,
-  UserCog,
-  LogOut,
-  MessageSquare,
-  UserRoundX,
-  LogIn,
-} from "lucide-react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogClose,
-} from "@/shared/shadcn-ui/dialog";
-import { Textarea } from "@/shared/shadcn-ui/textarea";
-import { logOut, deleteUserAccount } from "@/api/auth";
+import { Moon, Sun, UserRound, LogOut, LogIn, Settings } from "lucide-react";
+import { logOut } from "@/api/auth";
 import Alert from "@/widgets/Alert";
 
 export default function ProfileButton({
@@ -42,48 +20,19 @@ export default function ProfileButton({
 }: {
   userInfo: LoginUserInfo;
 }) {
-  const navigate = useNavigate();
-  const [isProfileEditDialogOpen, setIsProfileEditDialogOpen] = useState(false);
-  const [isFeedbackDialogOpen, setIsFeedbackDialogOpen] = useState(false);
   const { theme, setTheme } = useTheme();
-
-  const [feedback, setFeedback] = useState("");
-
-  const sendFeedbackMutation = useMutation({
-    mutationFn: () => sendUserFeedback(feedback),
+  const { mutate: logOutMutation } = useMutation({
+    mutationFn: logOut,
     onSuccess: () => {
-      toast.success("피드백이 전송되었습니다.");
-      setFeedback("");
-      setIsFeedbackDialogOpen(false);
+      window.location.href = `/`;
     },
   });
-
-  const handleSubmitFeedback = async (e: React.FormEvent) => {
-    e.preventDefault();
-    await sendFeedbackMutation.mutateAsync();
-  };
 
   /**
    * 테마 토글 함수
    */
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
-  };
-
-  /**
-   * 로그아웃 함수
-   */
-  const handleLogOut = async () => {
-    await logOut();
-    window.location.href = `/`;
-  };
-
-  /**
-   * 회원 탈퇴 함수
-   */
-  const handleDelete = async () => {
-    await deleteUserAccount();
-    window.location.href = `/`;
   };
 
   return (
@@ -117,11 +66,13 @@ export default function ProfileButton({
             <>
               {/* 로그인 버튼 */}
               <DropdownMenuItem
-                onClick={() => navigate("/login")}
                 className="flex items-center justify-start gap-2"
+                asChild
               >
-                <LogIn className="h-5 w-5" />
-                <span className="text-xs">로그인</span>
+                <Link to="/login">
+                  <LogIn className="h-5 w-5" />
+                  <span className="text-xs">로그인</span>
+                </Link>
               </DropdownMenuItem>
             </>
           ) : (
@@ -137,20 +88,22 @@ export default function ProfileButton({
                 </Link>
               </DropdownMenuItem>
 
-              {/* 프로필 편집 버튼 */}
+              {/* 설정 버튼 */}
               <DropdownMenuItem
-                onClick={() => setIsProfileEditDialogOpen(true)}
                 className="flex items-center justify-start gap-2"
+                asChild
               >
-                <UserCog className="h-5 w-5" />
-                <span className="text-xs">프로필 편집</span>
+                <Link to="/settings">
+                  <Settings className="h-5 w-5" />
+                  <span className="text-xs">설정</span>
+                </Link>
               </DropdownMenuItem>
 
               {/* 로그아웃 버튼 */}
               <Alert
                 title="로그아웃하시겠습니까?"
                 description="로그아웃 시 서비스를 이용하려면 다시 로그인이 필요합니다."
-                onConfirm={handleLogOut}
+                onConfirm={logOutMutation}
               >
                 <DropdownMenuItem
                   onSelect={(e) => {
@@ -162,84 +115,10 @@ export default function ProfileButton({
                   <span className="text-xs">로그아웃</span>
                 </DropdownMenuItem>
               </Alert>
-
-              {/* 피드백 버튼 */}
-              <DropdownMenuItem
-                onClick={() => setIsFeedbackDialogOpen(true)}
-                className="flex items-center justify-start gap-2"
-              >
-                <MessageSquare className="h-5 w-5" />
-                <span className="text-xs">피드백 보내기</span>
-              </DropdownMenuItem>
-
-              {/* 회원 탈퇴 버튼 */}
-              <Alert
-                title="서비스에서 탈퇴하시겠습니까?"
-                description="탈퇴 후 모든 데이터는 삭제되며 복구가 불가능합니다."
-                onConfirm={handleDelete}
-              >
-                <DropdownMenuItem
-                  onSelect={(e) => e.preventDefault()}
-                  className="flex items-center justify-start gap-2 text-destructive focus:text-destructive focus:bg-destructive/20 active:text-destructive active:bg-destructive/20"
-                >
-                  <UserRoundX className="h-5 w-5" />
-                  <span className="text-xs">회원 탈퇴</span>
-                </DropdownMenuItem>
-              </Alert>
             </>
           )}
         </DropdownMenuContent>
       </DropdownMenu>
-
-      <Dialog
-        open={isProfileEditDialogOpen}
-        onOpenChange={setIsProfileEditDialogOpen}
-      >
-        {/* [&>button]:hidden 은 창 닫기 버튼을 숨기는 설정이다 */}
-        <DialogContent className="max-w-sm [&>button]:hidden">
-          <DialogHeader className="text-left">
-            <DialogTitle>프로필 편집</DialogTitle>
-          </DialogHeader>
-
-          <DialogDescription hidden></DialogDescription>
-
-          {/* 프로필 편집 컴포넌트 */}
-        </DialogContent>
-      </Dialog>
-
-      <Dialog
-        open={isFeedbackDialogOpen}
-        onOpenChange={setIsFeedbackDialogOpen}
-      >
-        <DialogContent className="max-w-lg [&>button]:hidden">
-          <DialogHeader className="text-left">
-            <DialogTitle>피드백 보내기</DialogTitle>
-          </DialogHeader>
-          <DialogDescription>
-            서비스 이용 중 발생한 버그, 또는 불편했던 점들을 작성해서
-            보내주세요. 서비스 개선점에 대해서 자세하게 작성해주시면 더욱 좋은
-            서비스를 제공하기 위해 노력하겠습니다.
-          </DialogDescription>
-          <form onSubmit={handleSubmitFeedback} className="flex flex-col gap-4">
-            <Textarea
-              placeholder="피드백을 입력해주세요."
-              value={feedback}
-              rows={5}
-              onChange={(e) => setFeedback(e.target.value)}
-            />
-            <DialogFooter className="flex flex-row justify-end gap-2">
-              <DialogClose asChild>
-                <Button type="button" variant="outline">
-                  취소
-                </Button>
-              </DialogClose>
-              <Button type="submit" disabled={feedback.length === 0}>
-                제출
-              </Button>
-            </DialogFooter>
-          </form>
-        </DialogContent>
-      </Dialog>
     </>
   );
 }
