a1d499c548f2acc0640a2ec960795e8959fc92fc
Baejw0111 <bjw0111@naver.com>
2025-01-19 12:35:07 +0900

    style(FE): PageTemplate 패딩 업데이트로 레이아웃 개선
    - PageTemplate 컴포넌트의 패딩을 조정하여 간격을 개선, `py-8`에서 `pt-8 pb-16`으로 변경

--- front/src/shared/original-ui/PageTemplate.tsx
+++ front/src/shared/original-ui/PageTemplate.tsx
@@ -4,6 +4,8 @@ export default function PageTemplate({
   children: React.ReactNode;
 }) {
   return (
-    <div className="max-w-screen-2xl mx-auto px-4 md:px-6 py-8">{children}</div>
+    <div className="max-w-screen-2xl mx-auto px-4 md:px-6 pt-8 pb-16">
+      {children}
+    </div>
   );
 }
